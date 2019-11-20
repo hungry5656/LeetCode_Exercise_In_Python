7435451b50be522040c3b352cb7cf265ddadb15a
hungry5656 <2863880262@qq.com>
2019-11-20 09:56:52 +0800

    delete unnecessary expression

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -5,7 +5,6 @@ class Solution(object):
         :rtype: int
         """
         Roman_List = {'I':1,'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M': 1000}
-        check_reverse = False
         sum_n = 0
         ls = []
         current_c = 0
